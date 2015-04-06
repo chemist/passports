50d8750eb502bde74042766adf0e51b9d64e59c8
Alexey Smirnov <chemistmail@gmail.com>
2015-04-06 17:15:53 +0300

    int

--- src/Check.hs
+++ src/Check.hs
@@ -13,6 +13,10 @@ main = main' =<< getArgs
   where
     main' [t] = case parseOnly input (pack t) of
                      Left e -> error e
-                     Right p -> print =<< check p
+                     Right p -> print =<< boolToInt `fmap` check p
     main' _ = error "passports_check NUM"
                      
+
+boolToInt :: Bool -> Int
+boolToInt True = 1
+boolToInt _    = 0
