f6d981b6ce8192b439249950d87eef032b9aeeac
Alexey Smirnov <chemistmail@gmail.com>
2016-03-10 18:17:09 +0300

    fix

--- web/src/Main.purs
+++ web/src/Main.purs
@@ -3,10 +3,9 @@ module Main where
 import Prelude (Unit, (>>=), bind, return, ($), unit, (<$>))
 import Control.Apply ((*>))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Console (CONSOLE, print)
 import DOM  (DOM)
-import React (ReactElement, ReactClass, createFactory, createClass, spec, readState)
-import ReactDOM (render)
+import React (ReactElement, ReactClass, createFactory, createClass, spec, readState, render, transformState)
 import React.DOM as D
 import React.DOM.Props as P
 -- import Data.Maybe
@@ -19,9 +18,10 @@ import DOM.HTML.Document (body)
 import DOM.HTML.Types (htmlElementToElement)
 import DOM.HTML.Window (document)
 import DOM.Node.Types (Element())
+import Unsafe.Coerce
 
 main :: forall eff. Eff (ajax :: AJAX, dom :: DOM, console :: CONSOLE | eff) Unit
-main = component myApp "hello"
+main = component myApp unit
 
 component :: forall props eff. ReactClass props -> props -> Eff ( ajax :: AJAX, console :: CONSOLE, dom :: DOM | eff ) Unit
 component app props = (container >>= render ui) *> return unit
@@ -30,6 +30,11 @@ component app props = (container >>= render ui) *> return unit
       ui = D.div [ P.className "root" ]
                  [ createFactory app props ]
 
+type Hello = { hello :: String }
+
+type Passport = { passport :: String, status :: Boolean }
+type Passports = { raw :: String, state :: Array Passport }
+
 container :: forall eff. Eff (ajax :: AJAX, console :: CONSOLE, dom :: DOM | eff) Element
 container = do
   win <- window
@@ -37,9 +42,28 @@ container = do
   elm <- fromJust <$> toMaybe <$> body doc
   return $ htmlElementToElement elm
 
-myApp :: ReactClass String
-myApp = createClass (spec {} render)
+myApp :: ReactClass Unit
+myApp = createClass (spec { raw : "", state : []} render)
   where
       render this = do
           st <- readState this
-          return $ D.text "hello world"
+          return $ 
+            D.div'
+              [ D.p' [ D.text "Введите паспорта" ] 
+              , D.div'
+                [ D.button [ P.className "check" ]
+                           [ D.text "Проверить" ]
+                , D.button [ P.className "clean" ]
+                           [ D.text "Очистить" ]
+                ]
+              , D.ul' [ D.li' [ ]]
+              , D.textarea [ P.className "bulk-body"
+                           , P._id "bulk-body"
+                           , P.cols "40"
+                           , P.rows "3"
+                           , P.onChange $ \e -> do
+                               let text = (unsafeCoerce e).target.value
+                               transformState this $ \st -> { raw : text, state : st.state }
+                               print text
+                           ] [ D.text "" ]
+              ]
